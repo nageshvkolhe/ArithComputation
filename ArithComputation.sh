c6a44155f3363b702b529af01125454bd3b6076b
gitbash <kolhenagesh2017@gmail.com>
2021-04-30 16:17:16 +0530

    Modified:Desending order

--- ArithComputation.sh
+++ ArithComputation.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
-
-
- declear -A arrayName
-
 echo "Please enter your first number: "
 read a
 echo "Enter second number: "
 read b
 echo "Enter third number: "
 read c
-
+ declear -A arrayName
 p=$(( a + b * c ))
 echo "p:$p"
-
 s=$(( a * b + c ))
 echo "s:$s"
-
+r=$(( a % b + c ))
+echo "r:$r"
 q=$(( c + a / b ))
 echo "q:$q"
-
-t=$(( a % b + c ))
-echo "t:$t"
-
-
-t=$(( a % b + c ))
-echo "t:$t"
-
-
 arrayName=( $p $q $r $s )
 
-t=$(( a % b + c ))
-echo "t:$t"
 echo ${arrayName[@]}
 
 
+
 for ((i = 0; i<4; i++))
+for (( i = 0; i <4 ; i++ ))
+do
+for (( j =1; j <4; j++ ))
+do
+if [ ${arrayName[i]} -lt ${arrayName[j+1]} ]; then
+temp=${arrayName[i]}
+arrayName[$i]=${arrayName[j+1]}
+arrayName[$j+1]=$temp
+fi
+done
+done
+
+echo "Sorted Computation for descending order"
+
+for (( i=0; i <4; i++ ))
 do
 
     for((j = 0; j<4-i-1; j++))
     do
 
         if [ ${arrayName[j]} -gt ${arrayName[$((j+1))]} ]
-	@@ -43,4 +44,4 @@ do
+        then
+            # swap
+            temp=${arrayName[j]}
+            arrayName[$j]=${arrayName[$((j+1))]}  
+            arrayName[$((j+1))]=$temp
+        fi
+    done
+echo ${arrayName[$i]}
 done
 
 echo "Array in sorted order :"
